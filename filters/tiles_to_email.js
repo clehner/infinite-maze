fde6786abc6b2e70b2d5eeaaeec67b8bed403036
Charles Lehner <celehner1@gmail.com>
2011-01-03 00:40:55 -0500

    Improve welcome email. Queue outgoing emails.

--- filters/tiles_to_email.js
+++ filters/tiles_to_email.js
@@ -1,5 +1,5 @@
 function (doc, req) {
 	return (doc.type == "tile") &&
 		!doc.emailed_neighbors &&
-		doc.created_at > 1292446326058; // don't email old tiles
+		doc.created_at > 1293725270810; // don't email old tiles
 }
--- nodejs/emails.js
+++ nodejs/emails.js
@@ -2,10 +2,36 @@ var sys = require('sys'),
 	couchdb = require('./node-couchdb'),
 	cred = require('./credentials');
 	mail = require('./node-mail/lib/mail').Mail(cred.mail),
-	client = couchdb.createClient(5984, cred.couchdb.host, 
+	client = couchdb.createClient(cred.couchdb.port || 5984, cred.couchdb.host, 
 		cred.couchdb.user, cred.couchdb.pass),
 	db = client.db('maze'),
-	debug = cred.debug;
+	debug = cred.debug,
+	wait = 4000; // ms in between emails
+
+// queuing sending mails
+
+var _queued = [];
+var queueing = false;
+function sendLatest() {
+	var args = _queued.pop();
+	if (args) {
+		setTimeout(sendLatest, wait);
+		args[0].apply(null, args[1]);
+	} else {
+		queueing = false;
+	}
+}
+function queue(func /*, args... */) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	sys.debug("Queueing message.");
+	_queued.push([func, args]);
+	if (!queueing) {
+		queueing = true;
+		sendLatest();
+	}
+}
+
+// db stuff
 
 function saveDoc(doc, cb) {
 	if (debug) cb();
@@ -20,7 +46,7 @@ db.changesStream({
 	doc.emailed_welcome = true;
 	saveDoc(doc, function (er, ok) {
 		if (er) throw er;
-		sendNewUserEmail(doc);
+		queue(sendNewUserEmail, doc);
 	});
 });
 
@@ -55,7 +81,7 @@ db.changesStream({
 							if (row) {
 								var email = row.value;
 								// Send an email
-								sendNewTileEmail(doc, username, email);
+								queue(sendNewTileEmail, doc, username, email);
 							}
 						});
 					}
@@ -65,7 +91,9 @@ db.changesStream({
 	});
 });
 
-var debugAddress = debug && 'mazemaster.cel@lehnerstudios.com';
+// sending the mails
+
+var debugAddress = debug && 'theinfinitemaze-debug.cel@lehnerstudios.com';
 var sender = cred.mail.address;
 var siteRoot = 'http://www.theinfinitemaze.com/';
 
@@ -79,18 +107,20 @@ function sendNewUserEmail(user) {
 		subject: 'Welcome to the Infinite Maze!',
 		'Content-Type': 'text/html'
 	})
-	.body('Welcome to the Infinite Maze!<br>\n' +
-		'<a href="' + siteRoot + '">www.theinfinitemaze.com</a><br>\n<br>\n' +
-		'You signed up with the username "' + name + '".<br>\n<br>\n' +
-		'<img src="' + siteRoot + 'images/welcome-email-logo.png" alt="TheInfiniteMaze.com">')
+	.body('Welcome to The Infinite Maze, the collaborative maze drawing site!<br><br>Your username is "' + name + '". You signed up a while ago, but this your official welcome email.<br><br>New features added recently:<br><br>- <b>More colors</b> to choose from. And a bucket tool.<br>- <b>You don\'t have to start over.</b> When you come back to the site, you now continue from where you were last time.<br>- <b>Teleport to your tiles</b> - You can now jump directly to any tile you have drawn. Just scroll to it and then click the green circle to teleport to it.<br><br>Have fun!<br><br><a href="' + siteRoot + '">www.theinfinitemaze.com<br><br><img src="' + siteRoot + 'images/welcome-email-logo.png" alt="TheInfiniteMaze.com"></a>')
+	//.body('Welcome to The Infinite Maze, the collaborative maze drawing site.<br><br>You signed up with the username "cel".<br><br>Have fun!<br><br><a href="' + siteRoot + '">www.theinfinitemaze.com<br><br><img src="' + siteRoot + 'images/welcome-email-logo.png" alt="TheInfiniteMaze.com"></a>')
 	.send(function (er) {
-		if (er) throw er;
+		if (er) {
+			sys.debug('Error to '+username+': '+er);
+			throw er;
+		}
 		sys.debug('Sent!');
 	});
 }
 
 function sendNewTileEmail(tile, username, email) {
-	sys.puts("Sending new email to " + username + ": " + JSON.stringify(tile));
+	sys.puts("Sending new tile email to " + username + (debug ? " (debug)" : "")
+		+ ": " + JSON.stringify(tile));
 	var link = siteRoot + '#' + tile.location.join(',');
 	mail.message({
 		from: sender,
@@ -98,13 +128,12 @@ function sendNewTileEmail(tile, username, email) {
 		subject: 'New maze drawing',
 		'Content-Type': 'text/html'
 	})
-	.body('Hi ' + username + ',<br>\n<br>\n' +
-		'"' + tile.creator + '" ' +
-		'drew something next to one of your drawings. ' +
-		'<a href="' + link + '">See what they drew.</a><br>\n<br>\n' +
-		'- TheInfiniteMaze.com')
+	.body('Hi ' + username + ',<br><br>"' + tile.creator + '" drew something next to one of your drawings in the maze.<br><br><a href="' + link + '">See what they drew.</a><br><br>-TheInfiniteMaze.com')
 	.send(function (er) {
-		if (er) throw er;
+		if (er) {
+			sys.debug('Error on tile to '+username+': '+er);
+			throw er;
+		}
 		sys.debug('Sent!');
 	});
 }
