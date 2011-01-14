8e74d13a1ca92e8116fdcaaa3a309689698612a4
Charles Lehner <celehner1@gmail.com>
2011-01-14 16:17:32 -0500

    Start tiles_by_creator view at 0.

--- views/tiles_by_creator/map.js
+++ views/tiles_by_creator/map.js
@@ -1,8 +1,11 @@
 function (doc) {
 	if (doc.type == 'tile') {
-		emit([doc.maze_id, doc.creator], {
+		emit([doc.maze_id, doc.creator], 1);
+		/*{
 			id: doc._id,
 			location: doc.location
-		});
+		}*/
+	} else if (doc.type == 'user-info') {
+		emit(["1", doc.name], 0);
 	}
 }
