05a3d7b4be83f6b0020ec46eb1d663688b29ec41
Charles Lehner <celehner1@gmail.com>
2012-09-01 15:37:18 -0400

    Leaderboard sort by image content contribution
    with penalty for empty tiles.

--- views/tile_content_by_creator/map.js
+++ views/tile_content_by_creator/map.js
@@ -1,8 +1,8 @@
 function (doc) {
 	if (doc.type == 'tile') {
 		var size = doc._attachments['tile.png'].length;
-		emit([doc.maze_id, doc.creator], size/1000);
+		emit([doc.maze_id, doc.creator], [Math.max(size - 1665, 100)/1000, size/1000, 1]);
 	} else if (doc.type == 'user-info') {
-		emit(["1", doc.name], 0);
+		emit(["1", doc.name], [0, 0, 0]);
 	}
 }
--- views/tile_content_by_creator/reduce.js
+++ views/tile_content_by_creator/reduce.js
@@ -1,1 +1,10 @@
-_sum
+function(keys, values, rereduce) {
+	var result = values.shift() || [0, 0, 0];
+	for (var i = 0; i < values.length; i++) {
+		var value = values[i];
+		result[0] += value[0];
+		result[1] += value[1];
+		result[2] += value[2];
+	}
+	return result;
+}
