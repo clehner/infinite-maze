7c783ec426b10d83dcb7245cc639b642cb0c1078
Charles Lehner <cel@celehner.com>
2015-05-07 22:02:30 -0400

    Increase log verbosity

--- nodejs/notifications/new_neighbors.js
+++ nodejs/notifications/new_neighbors.js
@@ -1,6 +1,6 @@
 function sendNewTileEmail(api, tile, username, email) {
 	console.log("Sending new tile email to", username, '<' + email + '>',
-		tile.location);
+		tile.location.join(','), 'by', tile.creator);
 	api.sendMail([email], api.render('new_neighbor.txt', {
 		user: {
 			name: username,
